a8df34a5412c4308d179ab0d58e1e7b13fccd337
yexuan <yexuan@yexuan.site>
2018-10-28 21:06:00 +0800

    add function upload files return filename...

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express=require('express');
 const bodyParser=require('body-parser');
+const multer=require('multer');
+const fs=require('fs');
+const path=require('path');
 const wx_api=require('./interface/wx_api/wx_api')
 const tuling_api=require('./interface/tuling_api/tuling_api')
 const users=require('./interface/users/users')
 const mail=require('./mail/mail')
 
+var objmulter=multer({dest:"./www/upload"});    //dest指定上传文件地址
+var pathlib=path;
+
 var server=express();
+server.use(bodyParser.urlencoded({extended:false}));
+server.use(objmulter.any());
+server.use(express.static(__dirname));
 server.listen(11111);
 
 process.on('uncaughtException', function (err) {
@@ -38,3 +47,20 @@ server.use('/mail',function(req,res){
     if(req.query.judge==0)  mail.register(req,res); 
     if(req.query.judge==null) res.redirect('./WWW/404/QYZQ.html');
 })
+
+server.post('/UploadServlet',function(req,res){     //获取前台用户上传的头像订单
+    console.log(req.files);
+    //获取原始文件扩展名
+    var newName=req.files[0].path+pathlib.parse(req.files[0].originalname).ext;
+    console.log(pathlib.parse(req.files[0].originalname).ext);      //输出文件后缀
+    console.log("--->",newName);
+    fs.rename(req.files[0].path,newName,function(err){
+        if(err){
+            console.log("上传失败");
+            res.send(JSON.parse(`{ "file upload success ?": "flase" }`))
+        }else{
+            console.log("上传成功");
+            res.send(JSON.parse(`{ "file upload success ?": "true" ,"filename":"${newName}"}`))
+        }
+    })
+});
